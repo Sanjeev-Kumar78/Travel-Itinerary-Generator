db26f74f0c93d0b93b37c830539847625f80b2ac
Sanjeev <premchandrasingh786@gmail.com>
2024-10-02 19:07:33 +0530

    Model Updated

--- bard.py
+++ bard.py
@@ -9,12 +9,12 @@ palm_api_key = os.environ.get("PALM_API_KEY")
 
 # Create a config.
 palm.configure(api_key=palm_api_key)
-model = palm.GenerativeModel(model_name="gemini-pro")
+model = palm.GenerativeModel(model_name="gemini-1.5-flash-8b-exp-0924")
 # print(list(palm.list_models()))
 
 
 # Generate some text.
 def generate_itinerary(source, destination, start_date, end_date, no_of_day):
-    prompt = f"Generate a personalized trip itinerary for a {no_of_day}-day trip {source} to {destination} from {start_date} to {end_date}, with an optimum budget (Currency:INR)."
+    prompt = f"Generate a personalized trip itinerary for a {no_of_day}-day trip from {source} to {destination} on {start_date} to {end_date}, with an optimum budget (Currency:INR)."
     response = model.generate_content(prompt)
     return(response.text)
